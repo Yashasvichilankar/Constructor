5285bd55ccf9aa8f3862b6c41543af9297ba2c65
Yashasvi <yashasvichilankar27@gmail.com>
2023-03-20 22:10:45 +0530

    Adding file

---
+++ PrintNumber.java
@@ -0,0 +1,31 @@
+
+public class PrintNumber {
+void printn(int a)
+{
+	System.out.println("Integer: "+a);
+}
+void printn(float a)
+{
+	System.out.println("Float: "+a);
+}
+void printn(String a)
+{
+	System.out.println("String: "+a);
+}
+void printn(char a)
+{
+	System.out.println("Character: "+a);
+}
+void printn(double a)
+{
+	System.out.println("Double: "+a);
+}
+public static void main(String[] args) {
+	PrintNumber p1=new PrintNumber();
+	p1.printn(5);
+	p1.printn(2.2);
+	p1.printn("yashasvi");
+	p1.printn("A");
+	p1.printn(12.354);
+}
+}
